a51d89d7f4f38d917488a8fef79d733d14caae06
Md Tanvir Ahmed <155286588+tanvir1Ahmed@users.noreply.github.com>
2025-02-23 16:27:38 +0600

    Update Program.cs

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -6,7 +6,7 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello .C# Academy!!");
+            Console.WriteLine("Hello .C# Academy!! One more modification");
         }
     }
 }
